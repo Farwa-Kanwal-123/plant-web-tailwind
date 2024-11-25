e142fad4e37213588dc6eb94d7ceed46c4d5f1ba
Farwa-Kanwal-123 <wahabzahid06@gmail.com>
2024-11-25 22:06:00 +0500

    modify hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,7 @@
 import Image from "next/image";
+import {Sevillana } from "next/font/google"
+
+const sevillana = Sevillana({subsets:['latin'],weight:"400"})
 
 const Hero = () => {
   return (
@@ -21,15 +24,16 @@ const Hero = () => {
           className="block md:hidden"
         />
 
-        <div className="absolute inset-0 bg-black/50 flex justify-center">
+        <div className="absolute inset-0 bg-black/40 flex justify-center">
           <div className="flex justify-center">
-            <div className="flex flex-col items-center justify-center text-white space-y-8 md:space-y-10 lg:space-y-14 font-bold font-serif "
+            <div className="flex flex-col items-center justify-center text-white space-y-6 md:space-y-8 lg:space-y-10 font-serif "
             >
               <div className="text-sm md:text-base lg:text-xl">WELCOME TO THE GREENSTORE </div>
-              <h2 className="text-2xl md:text-3xl lg:text-4xl text-center px-3 text-wrap">
-                Let&#39;s Bring the Spring to Your Home{" "}
-              </h2>
-              <button className="bg-green-600 text-white px-2 md:px-5 lg:px-6 py-1 md:py-2 rounded-md hover:scale-105">
+              <div className={`${sevillana.className} text-2xl md:text-3xl lg:text-4xl text-center px-3 text-wrap space-y-4`}>
+                <p>Let&#39;s Bring the Spring to Your Home!{" "}</p>
+              </div>
+              <p className="px-[10%] md:px-40 text-wrap text-center">Explore our curated collection of beautiful indoor plants, designed to brighten your space and enhance your well-being. Whether you're a seasoned plant parent or just starting your green journey, find the perfect plant to suit your style and space. Start growing your green world today!</p>
+              <button className="bg-green-600 text-white px-2 md:px-5 lg:px-6 py-1 md:py-2 rounded-full hover:scale-105">
                 SHOP NOW
               </button>
             </div>
